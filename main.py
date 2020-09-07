4e21ea4bb71f02cd34b0b6759c35631fc6b8180b
ohnchen <ohmejona@gmail.com>
2020-09-07 22:29:37 +0200

    Adding comments

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+### Initialize the new TKinter window with an icon and a title
 root = Tk()
 root.title("Stimmungsthermometer")
 root.iconbitmap("c:/temper/IconThermometer.ico")    
 
+### Setting n to 1 so the "heat increases" at the first button push
 n = 1 
+### Defining the function for increasing the heat
 def increase():
     global n 
     if n == 1:
@@ -23,7 +26,7 @@ def increase():
     else:
         thermo_label.config(image=thermo_5)
 
-
+### Defining the function for decreasing the heat
 def decrease():
     global n 
     if n == 5:
@@ -41,14 +44,15 @@ def decrease():
     else:
         thermo_label.config(image=thermo_1)
 
+### Assinging the Images to variables
 thermo_1 = ImageTk.PhotoImage(Image.open("Thermometer_1.png"))
 thermo_2 = ImageTk.PhotoImage(Image.open("Thermometer_2.png"))
 thermo_3 = ImageTk.PhotoImage(Image.open("Thermometer_3.png"))
 thermo_4 = ImageTk.PhotoImage(Image.open("Thermometer_4.png"))
-thermo_5 = ImageTk.PhotoImage(Image.open("Thermometer_5.png")) #.resize((450,660), Image.ANTIALIAS)
-thermo_label = Label(image=thermo_1)
+thermo_5 = ImageTk.PhotoImage(Image.open("Thermometer_5.png"))
 thermo_label.grid(row=0, column=0)
 
+### Putting buttons on the TK window with their ability to execute on of the two functions
 increaser_button = Button(root, text="Increase the Heat!", command=increase)
 increaser_button.grid(row=1, column=0)
 decreaser_button = Button(root, text="Decrease the Heat!", command=decrease)
