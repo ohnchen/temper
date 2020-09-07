4626f7d6798b3e88a1ff3087716a58f56ec4dc1d
ohnchen <ohmejona@gmail.com>
2020-09-07 22:55:52 +0200

    changing title

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from PIL import ImageTk, Image
 
 ### Initialize the new TKinter window with an icon and a title
 root = Tk()
-root.title("Stimmungsthermometer")
+root.title("Planspiel: Europa")
 root.iconbitmap("c:/temper/IconThermometer.ico")    
+root.geometry("320x410")
 
 ### Setting n to 1 so the "heat increases" at the first button push
 n = 1 
@@ -50,12 +51,15 @@ thermo_2 = ImageTk.PhotoImage(Image.open("Thermometer_2.png"))
 thermo_3 = ImageTk.PhotoImage(Image.open("Thermometer_3.png"))
 thermo_4 = ImageTk.PhotoImage(Image.open("Thermometer_4.png"))
 thermo_5 = ImageTk.PhotoImage(Image.open("Thermometer_5.png"))
-thermo_label.grid(row=0, column=0)
+thermo_label = Label(image=thermo_1)
+thermo_label.grid(row=1, column=0)
 
-### Putting buttons on the TK window with their ability to execute on of the two functions
+### Putting buttons on the TK window with their ability to execute on of the two functions, also defining a title
+title_lbl = Label(root, text="Stimmungsthermometer der ersten Partei:")
+title_lbl.grid(row=0, column=0)
 increaser_button = Button(root, text="Increase the Heat!", command=increase)
-increaser_button.grid(row=1, column=0)
+increaser_button.grid(row=2, column=0)
 decreaser_button = Button(root, text="Decrease the Heat!", command=decrease)
-decreaser_button.grid(row=2, column=0)
+decreaser_button.grid(row=3, column=0)
 
 root.mainloop()
